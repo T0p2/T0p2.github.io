fb632d40c78a9c9aa597c66d1146ce120dd7a878
nacho <nachoayerbe2003@gmail.com>
2024-05-10 19:14:50 -0300

    modificaciones estilos

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
-
-
 
 var check=document.querySelector(".check");
 check.addEventListener("click", idioma);
@@ -20,7 +5,7 @@ check.addEventListener("click", idioma);
 
 function idioma (){
     let id = check.checked;
-    if(id ==true){
+    if(id == true){
         location.href = "/index.html";
     }else{
         location.href= "/indexx.html";
